236d6d4b87ffda160b60df2f5f452182ea9e682e
javiergracia03 <javiergracia2003@gmail.com>
2022-03-31 13:46:43 +0200

    p2

--- p1/src/App.java
+++ p1/src/App.java
@@ -6,5 +6,6 @@ package p1.src;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World! 2 ");  
     }
 }
